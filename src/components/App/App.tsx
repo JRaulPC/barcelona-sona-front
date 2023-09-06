3bb04b6989462de09f32e0da7d0b25962e7ee627
Raúl Pérez <jrpcassone@gmail.com>
2023-09-06 13:57:06 +0200

    Implement login and logout functions with useAuthState hook

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,9 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Header from "../Header/Header";
 import NavigationBar from "../NavigationBar/NavigationBar";
-
 import "./App.css";
 import paths from "../../paths/paths";
+import HomePage from "../pages/HomePage/HomePage";
 
 const App = (): React.ReactElement => {
   return (
@@ -11,7 +11,8 @@ const App = (): React.ReactElement => {
       <Header />
       <Routes>
         <Route path="/" element={<Navigate to={paths.homePage} />} />
-        <Route path="/home" element={<Navigate to={paths.homePage} />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path={paths.homePage} element={<HomePage />} />
       </Routes>
       <NavigationBar />
     </div>
--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,51 +1,69 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import "./NavigationBar.css";
 import Button from "../Button/Button";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../firebase";
+import { signOut } from "firebase/auth";
 
 const NavigationBar = (): React.ReactElement => {
+  const [user] = useAuthState(auth);
+
+  const navigate = useNavigate();
+
+  const logout = async () => {
+    await signOut(auth);
+    navigate("/home");
+  };
+
   return (
-    <nav className="navigation-bar">
-      <ul className="navigation-bar__elements">
-        <li className="navigation-bar__element">
-          <NavLink to="/espacios" className="navigation-bar__link">
-            <img
-              src="/img/icon-list.png"
-              alt="Link para acceder a la página de listado"
-              width="24"
-              height="24"
-              className="navigation-bar__spots-logo"
-            />
-          </NavLink>
-          Espacios
-        </li>
-        <li className="navigation-bar__element">
-          <NavLink to="/añadir" className="navigation-bar__link">
-            <img
-              src="/img/icon-add.png"
-              alt="Link para acceder a la página añadir un espacio"
-              width="24"
-              height="24"
-              className="navigation-bar__-logo"
-            />
-          </NavLink>
-          Añadir
-        </li>
-        <li className="navigation-bar__element">
-          <Button actionOnClick={() => {}} className="exit-button">
-            <>
-              <img
-                src="/img/icon-exit.png"
-                alt="Botón para cerrar sesión de usuario"
-                width="24"
-                height="24"
-                className="navigation-bar__exit-logo"
-              />
-              <span>Salir</span>
-            </>
-          </Button>
-        </li>
-      </ul>
-    </nav>
+    <>
+      {user ? (
+        <nav className="navigation-bar">
+          <ul className="navigation-bar__elements">
+            <li className="navigation-bar__element">
+              <Button actionOnClick={logout} className="exit-button">
+                <>
+                  <img
+                    src="/img/icon-exit.png"
+                    alt="Botón para cerrar sesión de usuario"
+                    width="24"
+                    height="24"
+                    className="navigation-bar__exit-logo"
+                  />
+                  <span>Salir</span>
+                </>
+              </Button>
+            </li>
+            <li className="navigation-bar__element">
+              <NavLink to="/añadir" className="navigation-bar__link">
+                <img
+                  src="/img/icon-add.png"
+                  alt="Link para acceder a la página añadir un espacio"
+                  width="24"
+                  height="24"
+                  className="navigation-bar__-logo"
+                />
+              </NavLink>
+              Añadir
+            </li>
+            <li className="navigation-bar__element">
+              <NavLink to="/espacios" className="navigation-bar__link">
+                <img
+                  src="/img/icon-list.png"
+                  alt="Link para acceder a la página de listado"
+                  width="24"
+                  height="24"
+                  className="navigation-bar__spots-logo"
+                />
+              </NavLink>
+              Espacios
+            </li>
+          </ul>
+        </nav>
+      ) : (
+        ""
+      )}
+    </>
   );
 };
 
