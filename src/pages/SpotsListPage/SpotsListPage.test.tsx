1c352f298c691195d23ec26d08eb76282d0303fd
Raúl Pérez <jrpcassone@gmail.com>
2023-09-07 12:15:07 +0200

    Fix tests to use spotsStore

--- src/pages/SpotsListPage/SpotsListPage.test.tsx
+++ src/pages/SpotsListPage/SpotsListPage.test.tsx
@@ -1,12 +1,24 @@
 import { render, screen } from "@testing-library/react";
 import SpotsListPage from "./SpotsListPage";
+import { setupStore } from "../../store";
+import { spotsMock } from "../../mocks/mocks";
+import { Provider } from "react-redux";
 
 describe("Given a Spots list page", () => {
   describe("When is rendered", () => {
     test("Then it should show a heading with the text 'Espacios'", () => {
       const headerText = "Espacios";
+      const store = setupStore({
+        spotsStore: {
+          spots: spotsMock,
+        },
+      });
 
-      render(<SpotsListPage />);
+      render(
+        <Provider store={store}>
+          <SpotsListPage />
+        </Provider>,
+      );
 
       const heading = screen.getByRole("heading", { name: headerText });
 
