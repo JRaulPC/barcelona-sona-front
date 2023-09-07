5ee69a77c7518a5f9c70611746b75828fb2fd3ca
Raúl Pérez <jrpcassone@gmail.com>
2023-09-07 11:32:52 +0200

    Dispatch spots in spots list page

--- src/pages/SpotsListPage/SpotsListPage.tsx
+++ src/pages/SpotsListPage/SpotsListPage.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
+import { useAppDispatch } from "../../store";
 import "./SpotsListPage.css";
+import { loadSpotsActionCreator } from "../../store/spots/spotsSlice";
+import { spotsMock } from "../../mocks/mocks";
 
 const SpotsListPage = (): React.ReactElement => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(loadSpotsActionCreator(spotsMock));
+  }, [dispatch]);
+
   return (
     <section className="spots-page">
       <h2 className="spots-page__heading">Espacios</h2>
