ca1a6984d1ce80ea4d89db7a168b72705315ba84
Raúl Pérez <jrpcassone@gmail.com>
2023-09-07 20:12:59 +0200

    Fix conditional rendering to happen in App component instead of navbar

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,13 +1,11 @@
 import { NavLink, useNavigate } from "react-router-dom";
 import "./NavigationBar.css";
 import Button from "../Button/Button";
-import { useAuthState } from "react-firebase-hooks/auth";
+
 import { auth } from "../../firebase";
 import { signOut } from "firebase/auth";
 
 const NavigationBar = (): React.ReactElement => {
-  const [user] = useAuthState(auth);
-
   const navigate = useNavigate();
 
   const logout = async () => {
@@ -16,54 +14,51 @@ const NavigationBar = (): React.ReactElement => {
   };
 
   return (
-    <>
-      {user ? (
-        <nav className="navigation-bar">
-          <ul className="navigation-bar__elements">
-            <li className="navigation-bar__element">
-              <Button actionOnClick={logout} className="exit-button">
-                <>
-                  <img
-                    src="/img/icon-exit.svg"
-                    alt="Botón para cerrar sesión de usuario"
-                    width="24"
-                    height="24"
-                    className="navigation-bar__exit-logo"
-                  />
-                  <span aria-label="Click para salir de la aplicación">
-                    Salir
-                  </span>
-                </>
-              </Button>
-            </li>
-            <li className="navigation-bar__element">
-              <NavLink to="/añadir" className="navigation-bar__link">
-                <img
-                  src="/img/icon-add.svg"
-                  alt="Link para acceder a la página añadir un espacio"
-                  width="24"
-                  height="24"
-                  className="navigation-bar__-logo"
-                />
-              </NavLink>
-              Añadir
-            </li>
-            <li className="navigation-bar__element">
-              <NavLink to="/espacios" className="navigation-bar__link">
-                <img
-                  src="/img/icon-list.svg"
-                  alt="Link para acceder a la página de listado"
-                  width="24"
-                  height="24"
-                  className="navigation-bar__spots-logo"
-                />
-              </NavLink>
-              Espacios
-            </li>
-          </ul>
-        </nav>
-      ) : null}
-    </>
+    <nav className="navigation-bar">
+      <ul className="navigation-bar__links">
+        <li className="navigation-bar__element">
+          <Button
+            actionOnClick={logout}
+            className="navigation-bar__exit-button"
+          >
+            <>
+              <img
+                src="/img/icon-exit.svg"
+                alt="Botón para cerrar sesión de usuario"
+                width="24"
+                height="24"
+                className="navigation-bar__exit-logo"
+              />
+              <span aria-label="Click para salir de la aplicación">Salir</span>
+            </>
+          </Button>
+        </li>
+        <li className="navigation-bar__element">
+          <NavLink to="/añadir" className="navigation-bar__link">
+            <img
+              src="/img/icon-add.svg"
+              alt="Link para acceder a la página añadir un espacio"
+              width="24"
+              height="24"
+              className="navigation-bar__logo"
+            />
+          </NavLink>
+          Crear
+        </li>
+        <li className="navigation-bar__element">
+          <NavLink to="/espacios" className="navigation-bar__link">
+            <img
+              src="/img/icon-list.svg"
+              alt="Link para acceder a la página de listado"
+              width="24"
+              height="24"
+              className="navigation-bar__spots-logo"
+            />
+          </NavLink>
+          Espacios
+        </li>
+      </ul>
+    </nav>
   );
 };
 
