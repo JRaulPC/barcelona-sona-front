2630d8cd78a26bae73758cde7574ae4c014d0249
Raúl Pérez <jrpcassone@gmail.com>
2023-09-09 17:12:36 +0200

    Correct spot  interface properties naming and SpotCard info css

--- src/mocks/mocks.ts
+++ src/mocks/mocks.ts
@@ -7,8 +7,8 @@ export const spotsMock: Spot[] = [
     imageUrl:
       "https://s2.qwant.com/thumbr/0x380/c/5/0a5db7bbec61444fb5119af51a87c45fd74dbaf7aac051669d6f06bb573b10/la-modelo-prison-hallway.jpg?u=https%3A%2F%2Ftheinternationalangle.com%2Fwp-content%2Fuploads%2F2018%2F01%2Fla-modelo-prison-hallway.jpg&q=0&b=1&p=0&a=0",
     spotUse: "Carcel",
-    opening: 1910,
-    visited: false,
+    openingYear: 1910,
+    isVisited: false,
   },
   {
     id: "2",
@@ -16,7 +16,7 @@ export const spotsMock: Spot[] = [
     imageUrl:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Palau_de_la_M%C3%BAsica_Catalana%2C_the_Catalan_Concert_Hall.jpg/800px-Palau_de_la_M%C3%BAsica_Catalana%2C_the_Catalan_Concert_Hall.jpg?20120927183654",
     spotUse: "Auditori",
-    opening: 1856,
-    visited: false,
+    openingYear: 1856,
+    isVisited: false,
   },
 ];
