ef52d69415b8f9425c38de8dc5c6125d2af15113
Raúl Pérez <jrpcassone@gmail.com>
2023-09-11 20:25:51 +0200

    Test uiSlice with start/stop loading actions

---
+++ src/store/ui/__tests__/loadingSlice.test.ts
@@ -0,0 +1,36 @@
+import { UiState } from "../types";
+import {
+  startLoadingActionCreator,
+  stopLoadingActionCreator,
+  uiReducer,
+} from "../uiSlice";
+
+describe("Given a ui reducer", () => {
+  describe("When it receives an uiState and a startLoading action with isLoading false", () => {
+    test("Then it should return a new state with the state isLoading as true", () => {
+      const currentUiState: UiState = {
+        isLoading: false,
+      };
+
+      const startLoadingAction = startLoadingActionCreator();
+
+      const newUiState = uiReducer(currentUiState, startLoadingAction);
+
+      expect(newUiState).toHaveProperty("isLoading", true);
+    });
+  });
+
+  describe("When it receives an uiState and a stop action with isLoading true", () => {
+    test("Then it should return a new state with the state isLoading as false", () => {
+      const currentUiState: UiState = {
+        isLoading: true,
+      };
+
+      const startLoadingAction = stopLoadingActionCreator();
+
+      const newUiState = uiReducer(currentUiState, startLoadingAction);
+
+      expect(newUiState).toHaveProperty("isLoading", false);
+    });
+  });
+});
