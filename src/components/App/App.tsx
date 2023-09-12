b3c00f0db20e3a3294d8365c61753531c21b2d64
Raúl Pérez <jrpcassone@gmail.com>
2023-09-12 13:03:34 +0200

    Add ErrorFeedback component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -9,35 +9,44 @@ import React, { Suspense } from "react";
 import "./App.css";
 import { LazySpotsListPage } from "../SpotsList/SpotsList";
 import { LazyNavigationBar } from "../NavigationBar/NavigationBar";
+import { useAppSelector } from "../../store";
+import ErrorFeedback from "../ErrorFeedback/ErrorFeedback";
+import Loader from "../Loader/Loader";
 
 const App = (): React.ReactElement => {
   const [user] = useAuthState(auth);
+  const isError = useAppSelector((state) => state.uiStore.isError);
+  const isLoading = useAppSelector((state) => state.uiStore.isLoading);
 
   return (
-    <div className="container">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Navigate to={paths.homePage} />} />
-        <Route path={paths.homePage} element={<HomePage />} />
+    <>
+      {isLoading && <Loader />}
+      {isError && <ErrorFeedback />}
+      <div className="container">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Navigate to={paths.homePage} />} />
+          <Route path={paths.homePage} element={<HomePage />} />
 
-        <Route
-          path={paths.spots}
-          element={
-            <ProtectedRoute>
-              <Suspense>
-                <LazySpotsListPage />
-              </Suspense>
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
+          <Route
+            path={paths.spots}
+            element={
+              <ProtectedRoute>
+                <Suspense>
+                  <LazySpotsListPage />
+                </Suspense>
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
 
-      {user && (
-        <Suspense>
-          <LazyNavigationBar />
-        </Suspense>
-      )}
-    </div>
+        {user && (
+          <Suspense>
+            <LazyNavigationBar />
+          </Suspense>
+        )}
+      </div>
+    </>
   );
 };
 
