be0692f3cec75d8b07c1121123b1de7584d93001
Raúl Pérez <jrpcassone@gmail.com>
2023-09-15 09:41:52 +0200

    Add form state and function to submit spot to db when button is clicked

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,18 +2,27 @@ import React, { PropsWithChildren } from "react";
 
 interface ButtonProps extends PropsWithChildren {
   className: string;
-  actionOnClick: () => void;
+  actionOnClick?: () => void;
+  disabled?: boolean;
+  type: "button" | "submit" | "reset";
 }
 
 const Button = ({
   className,
   actionOnClick,
   children,
+  disabled,
+  type,
 }: ButtonProps): React.ReactElement => {
   const buttonClass = `button ${className}`;
 
   return (
-    <button type="button" className={buttonClass} onClick={actionOnClick}>
+    <button
+      disabled={disabled}
+      type={type}
+      className={buttonClass}
+      onClick={actionOnClick}
+    >
       {children}
     </button>
   );
--- src/components/NewSpot/NewSpot.tsx
+++ src/components/NewSpot/NewSpot.tsx
@@ -1,33 +1,102 @@
+import { useEffect, useState } from "react";
+import { Spot } from "../../types";
 import Button from "../Button/Button";
 import "./NewSpot.css";
+import useSpotsApi from "../../hooks/useSpotsApi";
+import { useAppDispatch } from "../../store";
+import { addsSpotActionCreator } from "../../store/spots/spotsSlice";
+import { useNavigate } from "react-router-dom";
+import paths from "../../paths/paths";
 
 const NewSpot = (): React.ReactElement => {
+  const [disabled, setDisabled] = useState(true);
+  const [newSpot, setNewSpot] = useState<Partial<Spot>>({
+    name: "",
+    imageUrl: "",
+    spotUse: "",
+    openingYear: 0,
+    isVisited: false,
+  });
+  const dispatch = useAppDispatch();
+  const { addSpot } = useSpotsApi();
+
+  const changeNewSpot = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewSpot((newSpot) => ({
+      ...newSpot,
+      [event.target.id]: event.target.value,
+    }));
+  };
+
+  const { imageUrl, name, openingYear, spotUse } = newSpot;
+
+  useEffect(() => {
+    name !== "" && imageUrl !== "" && openingYear !== 0 && spotUse !== ""
+      ? setDisabled(false)
+      : setDisabled(true);
+  }, [name, imageUrl, openingYear, spotUse]);
+
+  const navigate = useNavigate();
+
+  const submitForm = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const spot: Spot = await addSpot(newSpot);
+
+    dispatch(addsSpotActionCreator(spot));
+    navigate(paths.spots);
+  };
+
   return (
-    <form className="form-spot">
+    <form className="form-spot" onSubmit={submitForm}>
       <div className="form-control">
-        <label htmlFor="spot">Espacio</label>
-        <input type="text" id="spot" maxLength={100} />
+        <label htmlFor="name">Espacio</label>
+        <input
+          type="text"
+          id="name"
+          maxLength={100}
+          value={newSpot.name}
+          onChange={changeNewSpot}
+        />
       </div>
       <div className="form-control">
-        <label htmlFor="image">URL Imagen</label>
-        <input type="text" id="image" />
+        <label htmlFor="imageUrl">URL Imagen</label>
+        <input
+          type="text"
+          id="imageUrl"
+          value={newSpot.imageUrl}
+          onChange={changeNewSpot}
+        />
       </div>
       <div className="form-control">
-        <label htmlFor="creation">Año de creación</label>
-        <input type="number" id="creation" min="1300" max="2023" />
+        <label htmlFor="openingYear">Año de creación</label>
+        <input
+          type="number"
+          id="openingYear"
+          min="1300"
+          max="2023"
+          maxLength={4}
+          onChange={changeNewSpot}
+        />
       </div>
       <div className="form-control">
-        <label htmlFor="spot-function">Función del espacio</label>
-        <input type="text" id="spot-function" />
+        <label htmlFor="spotUse">Función del espacio</label>
+        <input
+          type="text"
+          maxLength={50}
+          id="spotUse"
+          value={newSpot.spotUse}
+          onChange={changeNewSpot}
+        />
       </div>
       <div className="form-control__checkbox">
-        <label htmlFor="visited">Visitado</label>
-        <input className="form-control-check" type="checkbox" id="visited" />
+        <label htmlFor="isVisited">Visitado</label>
+        <input className="form-control-check" type="checkbox" id="isVisited" />
       </div>
       <div className="form-spot__button">
         <Button
-          actionOnClick={() => {}}
           className="outline-white outline-white--wider "
+          disabled={disabled}
+          type="submit"
         >
           Añadir espacio
         </Button>
