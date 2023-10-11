8cc8438b8e7c58797514ca2b606bb14b7acce665
Raúl Pérez <jrpcassone@gmail.com>
2023-10-11 12:51:02 +0200

    Add functionalities to header on desktop

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,65 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./Header.css";
 import paths from "../../paths/paths";
+import Button from "../Button/Button";
+import { signOut } from "firebase/auth";
+import { auth } from "../../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const Header = (): React.ReactElement => {
+  const [user] = useAuthState(auth);
+
+  const logout = async () => {
+    await signOut(auth);
+  };
+
   return (
     <header className="header">
-      <Link to={paths.spots}>
-        <h1 className="header__title">Barcelona SONA</h1>
-      </Link>
-      <img
-        className="header__logo"
-        src="/img/app-logo.svg"
-        alt="Barcelona sona logo"
-        width="62"
-        height="57"
-      />
+      <div className="header__page-links ">
+        <Link to={paths.spots}>
+          <h1 className="header__title">Barcelona SONA</h1>
+        </Link>
+        <ul className="header__links">
+          <li className="header__element">
+            <NavLink to={paths.createSpot} className="header__link">
+              {" "}
+              Crear
+            </NavLink>
+          </li>
+          <li className="header__element">
+            <NavLink to={paths.spots} className="header__link">
+              {" "}
+              Espacios
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="header__page-misc">
+        <span className="user-name">{user?.email}</span>
+        <Button
+          type="button"
+          actionOnClick={logout}
+          className="header__exit-button"
+        >
+          <>
+            <img
+              src="/img/icon-exit.svg"
+              alt="Botón para cerrar sesión de usuario"
+              width="24"
+              height="24"
+              className="header__exit-logo"
+            />
+            Salir
+          </>
+        </Button>
+        <img
+          className="header__logo"
+          src="/img/app-logo.svg"
+          alt="Barcelona sona logo"
+          width="62"
+          height="57"
+        />
+      </div>
     </header>
   );
 };
